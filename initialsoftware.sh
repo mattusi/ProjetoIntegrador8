e83aec48fe65bec39af69687aa9ff00aee256452
mattusi <matheustusi@gmail.com>
2021-11-22 18:22:14 -0300

    aaaaaaaa

--- initialsoftware.sh
+++ initialsoftware.sh
@@ -1,17 +1,20 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/bin/bash
 
 sudo apt-get update
 sudo apt-get -y install build-essential libssl-dev python-dev libffi-dev python-pip git
+sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get -y install libxvidcore-dev libx264-dev
+sudo apt-get -y install qt4-dev-tools libatlas-base-dev
+
 sudo pip3 install -r requirements.txt
+
+version=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+
+if [ $version == "3.7" ]; then
+pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl
+fi
+
+if [ $version == "3.5" ]; then
+pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp35-cp35m-linux_armv7l.whl
+fi
--- main.py
+++ main.py
@@ -10,7 +10,141 @@ import json
 import jwt
 import paho.mqtt.client as mqtt
 
+import cv2
+import numpy as np
+import sys
+import time
+from threading import Thread
+import importlib.util
 
+# Define VideoStream class to handle streaming of video from webcam in separate processing thread
+# Source - Adrian Rosebrock, PyImageSearch: https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
+class VideoStream:
+    """Camera object that controls video streaming from the Picamera"""
+    def __init__(self,resolution=(640,480),framerate=30):
+        # Initialize the PiCamera and the camera image stream
+        self.stream = cv2.VideoCapture(0)
+        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+        ret = self.stream.set(3,resolution[0])
+        ret = self.stream.set(4,resolution[1])
+            
+        # Read first frame from the stream
+        (self.grabbed, self.frame) = self.stream.read()
+
+	# Variable to control when the camera is stopped
+        self.stopped = False
+
+    def start(self):
+	# Start the thread that reads frames from the video stream
+        Thread(target=self.update,args=()).start()
+        return self
+
+    def update(self):
+        # Keep looping indefinitely until the thread is stopped
+        while True:
+            # If the camera is stopped, stop the thread
+            if self.stopped:
+                # Close camera resources
+                self.stream.release()
+                return
+
+            # Otherwise, grab the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+	# Return the most recent frame
+        return self.frame
+
+    def stop(self):
+	# Indicate that the camera and thread should be stopped
+        self.stopped = True
+
+global minimum_backoff_time
+global MAXIMUM_BACKOFF_TIME
+
+mqtt_bridge_port = 8883
+mqtt_bridge_hostname = 'mqtt.googleapis.com'
+cloud_region = 'us-central1'
+
+
+device_id = "raspi400"
+project_id = 'factory-automation-331600'
+message_type = 'state'
+
+registry_id = 'esteira'
+
+num_messages = 100
+
+private_key_file = '../.ssh/ec_private.pem'
+ca_certs = "../.ssh/roots.pem"
+algorithm = 'ES256'
+
+# Publish to the events or state topic based on the flag.
+sub_topic = "events" if message_type == "event" else "state"
+
+
+mqtt_topic = '/devices/{}/{}'.format(device_id, sub_topic)
+
+
+#OpenCV
+MODEL_NAME = "Sample_TFLite_model"
+GRAPH_NAME = "detect.tflite"
+LABELMAP_NAME = "labelmap.txt"
+min_conf_threshold = 0.5
+resW = "1280"
+resH = "720"
+imW, imH = int(resW), int(resH)
+
+
+# Import TensorFlow libraries
+# If tflite_runtime is installed, import interpreter from tflite_runtime, else import from regular tensorflow
+# If using Coral Edge TPU, import the load_delegate library
+pkg = importlib.util.find_spec('tflite_runtime')
+if pkg:
+    from tflite_runtime.interpreter import Interpreter
+else:
+    from tensorflow.lite.python.interpreter import Interpreter
+
+CWD_PATH = os.getcwd()
+
+# Path to .tflite file, which contains the model that is used for object detection
+PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,GRAPH_NAME)
+
+# Path to label map file
+PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,LABELMAP_NAME)
+
+# Load the label map
+with open(PATH_TO_LABELS, 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
+
+#load the tensorflow lite model
+interpreter = Interpreter(model_path=PATH_TO_CKPT)
+interpreter.allocate_tensors()
+
+# Get model details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+
+floating_model = (input_details[0]['dtype'] == np.float32)
+
+input_mean = 127.5
+input_std = 127.5
+
+# Initialize frame rate calculation
+frame_rate_calc = 1
+freq = cv2.getTickFrequency()
+
+# Initialize video stream
+videostream = VideoStream(resolution=(imW,imH),framerate=30).start()
+time.sleep(1)
+
+#---------------// OpenCV setup done //------------
+
+#Capturar sinal do sensor infravermelho
+
+#Start IoT MQTT STUFF
 logging.getLogger("googleapiclient.discovery_cache").setLevel(logging.CRITICAL)
 
 # The initial backoff time after a disconnection occurs, in seconds.
@@ -371,122 +505,125 @@ def send_data_from_bound_device(
     print("Finished.")
     # [END send_data_from_bound_device]
 
-
-def parse_command_line_args():
-    """Parse command line arguments."""
-    parser = argparse.ArgumentParser(description=(
-            'Example Google Cloud IoT Core MQTT device connection code.'))
-
-
-    parser.add_argument(
-            '--private_key_file',
-            required=True, help='Path to private key file.')
-
-    parser.add_argument(
-            '--message_type',
-            choices=('event', 'state'),
-            default='event',
-            help=('Indicates whether the message to be published is a '
-                  'telemetry event or a device state message.'))
-
-
-    parser.add_argument(
-            '--json_data_file',
-            default='data/SampleData.json',
-            help='Sample JSON file to stream the data from.')
-    return parser.parse_args()
-
-
 # [START iot_mqtt_run]
-def main():
-    global minimum_backoff_time
-    global MAXIMUM_BACKOFF_TIME
 
-    mqtt_bridge_port = 8883
-    mqtt_bridge_hostname = 'mqtt.googleapis.com'
-    cloud_region = 'us-central1'
 
+jwt_iat = datetime.datetime.utcnow()
+jwt_exp_mins = 20
+client = get_client(
+    project_id, cloud_region, registry_id, device_id,
+    private_key_file, algorithm, ca_certs,
+    mqtt_bridge_hostname, mqtt_bridge_port)
+#Start our infine loop
+i = 0
+while True:
+    i = i + 1
+    client.loop()
+
+        # Wait if backoff is required.
+    if should_backoff:
+        # If backoff time is too large, give up.
+        if minimum_backoff_time > MAXIMUM_BACKOFF_TIME:
+            print("Exceeded maximum backoff time. Giving up.")
+            break
+
+        # Otherwise, wait and connect again.
+        delay = minimum_backoff_time + random.randint(0, 1000) / 1000.0
+        print("Waiting for {} before reconnecting.".format(delay))
+        time.sleep(delay)
+        minimum_backoff_time *= 2
+        client.connect(mqtt_bridge_hostname, mqtt_bridge_port)
+
+    # Publish "payload" to the MQTT topic. qos=1 means at least once
+    # delivery. Cloud IoT Core also supports qos=0 for at most once
+    # delivery.
+    data_set = {"regId": registry_id, "devId": device_id, "num": i, "myData": random.randint(0, 100)}
+    payload = json.dumps(data_set)
     
-    device_id = "raspi400"
-    project_id = 'factory-automation-331600'
-    message_type = 'state'
-
-    registry_id = 'esteira'
-
-    num_messages = 100
-
-    private_key_file = '../.ssh/ec_private.pem'
-    ca_certs = "../.ssh/roots.pem"
-    algorithm = 'ES256'
-
-    # Publish to the events or state topic based on the flag.
-    sub_topic = "events" if message_type == "event" else "state"
-
-
-    mqtt_topic = '/devices/{}/{}'.format(device_id, sub_topic)
-
-    jwt_iat = datetime.datetime.utcnow()
-    jwt_exp_mins = 20
-    client = get_client(
-        project_id, cloud_region, registry_id, device_id,
-        private_key_file, algorithm, ca_certs,
-        mqtt_bridge_hostname, mqtt_bridge_port)
-
-    for i in range(num_messages + 1):
+    print('Publishing message #{}: \'{}\''.format(i, payload))
+        # [START iot_mqtt_jwt_refresh]
+    seconds_since_issue = (datetime.datetime.utcnow() - jwt_iat).seconds
+    if seconds_since_issue > 60 * jwt_exp_mins:
+        print("Refreshing token after {}s".format(seconds_since_issue))
+        jwt_iat = datetime.datetime.utcnow()
         client.loop()
+        client.disconnect()
+        client = get_client(
+            project_id,
+            cloud_region,
+            registry_id,
+            device_id,
+            private_key_file,
+            algorithm,
+            ca_certs,
+            mqtt_bridge_hostname,
+            mqtt_bridge_port,
+        )
+    # [END iot_mqtt_jwt_refresh]
+    # Publish "payload" to the MQTT topic. qos=1 means at least once
+    # delivery. Cloud IoT Core also supports qos=0 for at most once
+    # delivery.
+    client.publish(mqtt_topic, payload, qos=1)
+     #OpenCV Stuff
+    # Start timer (for calculating frame rate)
+    t1 = cv2.getTickCount()
 
-         # Wait if backoff is required.
-        if should_backoff:
-            # If backoff time is too large, give up.
-            if minimum_backoff_time > MAXIMUM_BACKOFF_TIME:
-                print("Exceeded maximum backoff time. Giving up.")
-                break
+    # Grab frame from video stream
+    frame1 = videostream.read()
 
-            # Otherwise, wait and connect again.
-            delay = minimum_backoff_time + random.randint(0, 1000) / 1000.0
-            print("Waiting for {} before reconnecting.".format(delay))
-            time.sleep(delay)
-            minimum_backoff_time *= 2
-            client.connect(mqtt_bridge_hostname, mqtt_bridge_port)
+    # Acquire frame and resize to expected shape [1xHxWx3]
+    frame = frame1.copy()
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb, (width, height))
+    input_data = np.expand_dims(frame_resized, axis=0)
 
-        # Publish "payload" to the MQTT topic. qos=1 means at least once
-        # delivery. Cloud IoT Core also supports qos=0 for at most once
-        # delivery.
-        data_set = {"regId": registry_id, "devId": device_id, "num": i, "myData": random.randint(0, 100)}
-        payload = json.dumps(data_set)
-        
-        print('Publishing message #{}: \'{}\''.format(i, payload))
-         # [START iot_mqtt_jwt_refresh]
-        seconds_since_issue = (datetime.datetime.utcnow() - jwt_iat).seconds
-        if seconds_since_issue > 60 * jwt_exp_mins:
-            print("Refreshing token after {}s".format(seconds_since_issue))
-            jwt_iat = datetime.datetime.utcnow()
-            client.loop()
-            client.disconnect()
-            client = get_client(
-                project_id,
-                cloud_region,
-                registry_id,
-                device_id,
-                private_key_file,
-                algorithm,
-                ca_certs,
-                mqtt_bridge_hostname,
-                mqtt_bridge_port,
-            )
-        # [END iot_mqtt_jwt_refresh]
-        # Publish "payload" to the MQTT topic. qos=1 means at least once
-        # delivery. Cloud IoT Core also supports qos=0 for at most once
-        # delivery.
-        client.publish(mqtt_topic, payload, qos=1)
+    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+    if floating_model:
+        input_data = (np.float32(input_data) - input_mean) / input_std
 
-        # Send events every second. State should not be updated as often
-        for i in range(0, 60):
-            time.sleep(1)
-            client.loop()
-    print('Finished.')
-# [END iot_mqtt_run]
+        # Perform the actual detection by running the model with the image as input
+    interpreter.set_tensor(input_details[0]['index'],input_data)
+    interpreter.invoke()
+    # Retrieve detection results
+    boxes = interpreter.get_tensor(output_details[0]['index'])[0] # Bounding box coordinates of detected objects
+    classes = interpreter.get_tensor(output_details[1]['index'])[0] # Class index of detected objects
+    scores = interpreter.get_tensor(output_details[2]['index'])[0] # Confidence of detected objects
+    #num = interpreter.get_tensor(output_details[3]['index'])[0]  # Total number of detected objects (inaccurate and not needed)
+
+    # Loop over all detections and draw detection box if confidence is above minimum threshold
+    for i in range(len(scores)):
+        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
+            # Get bounding box coordinates and draw box
+            # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+            ymin = int(max(1,(boxes[i][0] * imH)))
+            xmin = int(max(1,(boxes[i][1] * imW)))
+            ymax = int(min(imH,(boxes[i][2] * imH)))
+            xmax = int(min(imW,(boxes[i][3] * imW)))
+            
+            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+
+            # Draw label
+            object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+            label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+            label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+            cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+    # Draw framerate in corner of frame
+    cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
+
+    # All the results have been drawn on the frame, so it's time to display it.
+    cv2.imshow('Object detector', frame)
+
+    # Calculate framerate
+    t2 = cv2.getTickCount()
+    time1 = (t2-t1)/freq
+    frame_rate_calc= 1/time1
 
 
-if __name__ == '__main__':
-    main()
+
+
+# Clean up
+cv2.destroyAllWindows()
+videostream.stop()
+
